89dec293cadb58416df68983997239ce06999db5
rmamchyk <roman.mamchyk@gmail.com>
2019-12-07 19:55:04 +0200

    hidden dock icon

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let mainWindow;
 let tray;
 
 app.on('ready', () => {
+  app.dock.hide();
   mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true
@@ -19,6 +20,9 @@ app.on('ready', () => {
     show: false
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+  mainWindow.on('blur', () => {
+    mainWindow.hide();
+  });
 
   const iconName = isWindows ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);

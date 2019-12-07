e7457261ac765475a3069ffa52d9c9624c8ef804
rmamchyk <roman.mamchyk@gmail.com>
2019-12-07 20:38:33 +0200

    main window refactoring

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app } = require('electron');
 const path = require('path');
 const TimerTray = require('./app/TimerTray');
+const MainWindow = require('./app/MainWindow');
 
 const isWindows = process.platform === 'win32';
 let mainWindow;
@@ -9,20 +9,7 @@ let tray;
 
 app.on('ready', () => {
   app.dock.hide();
-  mainWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true
-    },
-    height: 500,
-    width: 300,
-    frame: false,
-    resizable: false,
-    show: false
-  });
-  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-  mainWindow.on('blur', () => {
-    mainWindow.hide();
-  });
+  mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
   const iconName = isWindows ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
